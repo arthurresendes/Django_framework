de19048c4d319219dddbf400afb9ae5cac5badf1
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-21 18:41:23 -0300

    Admin

--- Django1/core/admin.py
+++ Django1/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Produto,Cliente
 
-# Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome','preco','quantidade')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome','sobrenome','email','idade' )
+
+admin.site.register(Produto,ProdutoAdmin)
+admin.site.register(Cliente,ClienteAdmin)
+
--- Django1/core/models.py
+++ Django1/core/models.py
@@ -4,9 +4,15 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.FloatField('Preço')
     quantidade = models.IntegerField('Quantidade')
+    
+    def __str__(self):
+        return self.nome
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=200)
     sobrenome = models.CharField('Sobrenome', max_length=200)
     email = models.EmailField('Email', max_length=100)
     idade = models.IntegerField('Idade')
+    
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
