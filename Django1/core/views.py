e3d04474fc7651face16627804a2214cf91f916a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-21 19:41:12 -0300

    Django Shell + views logado + comandos Django

--- Django1/core/views.py
+++ Django1/core/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # print(dir(request.user))
+    if str(request.user) != 'AnonymousUser':
+        teste = f'Logado, usuario: {request.user
+        }'
+    else:
+        teste = 'Não logado'
+
     context ={
-        'Projeto': 'Meu primeiro projeto django'
+        'Projeto': 'Meu primeiro projeto django',
+        'logado': teste
     }
     return render(request, 'index.html',context)
 
