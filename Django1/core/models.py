fb87f00dc552a3079461ab5dceba28f20f9f9fe2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-21 18:07:41 -0300

    Models em Django

--- Django1/core/models.py
+++ Django1/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.FloatField('Preço')
+    quantidade = models.IntegerField('Quantidade')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=200)
+    sobrenome = models.CharField('Sobrenome', max_length=200)
+    email = models.EmailField('Email', max_length=100)
+    idade = models.IntegerField('Idade')
