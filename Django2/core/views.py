f52ffbf07303b9df08ec857a27f98b1bea6f44de
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-02-05 20:44:16 -0300

    Checked if request.method is POST and form submission validation with is_valid, messages: success and erros

--- Django2/core/views.py
+++ Django2/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ContatoForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,21 @@ def produto(request):
 
 def contato(request):
     form = ContatoForm()
+    
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+            print("Informações mensagem: ")
+            print(nome,email,assunto,mensagem)
+            
+            messages.success(request, "E-mail enviado com sucesso")
+            form = ContatoForm()
+        else:
+            messages.error(request, "Erro ao enviar e-mail")
     
     context = {
         'form': form
